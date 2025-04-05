672679c21a169b6569ff08f959534948f43e328d
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-04-05 22:23:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Herosection from './component/Herosection';
+import Navbar2 from './component/Navbar2';
+
+function App() {
+  return (
+    <>
+      <Navbar2 />
+      <Herosection />
+    </>
+
+  )
+}
+
+export default App
